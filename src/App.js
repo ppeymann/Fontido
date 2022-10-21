927d819603f84e926b2248a3b0ad87782d1a373c
peyman malek <p.malek32@gmail.com>
2022-10-21 21:31:10 +0330

    Route added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Story from "./pages/story/Story";
+import Register from "./components/register/Register";
+import Home from "./pages/Home/Home.js";
+import Video from "./pages/video/Video";
+import SubPage from "./pages/sub/Sub";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/story" element={<Story />} />
+        <Route path="/video" element={<Video />} />
+        <Route path="/login" element={<Register />} />
+        <Route path="/sub" element={<SubPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
